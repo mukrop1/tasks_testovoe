64057a2e2f8ee1a77a6b2b9ba60d1810196ed5b5
Sergey Kropachev <kropachev_1998@mail.ru>
2024-09-02 04:55:10 +0500

    models ready

--- models.py
+++ models.py
@@ -1,5 +1,17 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
 from database import Base
+
+# Определим модель Users - таблица юзеров
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Task", back_populates="owner")
 
 # Определяем модель Task - таблица заданий
 class Task(Base):
@@ -9,13 +21,6 @@ class Task(Base):
     title = Column(String, index=True)
     description = Column(String, index=True)
     completed = Column(Boolean, default=False)
-    
-# Определим модель Users - таблица юзеров
-# class Users(Base):
-#     __tablename__ = "users"
+    owner_id = Column(Integer, ForeignKey("user.id"))
 
-#     id = Column(Integer, primary_key=True)
-#     email = Column(String, nullable=False)
-#     username = Column(String, nullable=False)
-#     password = Column(String, nullable=False)
-#     role_id = Column(Integer, ForeignKey("tasks.id"))
+    owner = relationship("User", back_populates="items")
